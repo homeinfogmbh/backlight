f660a3ecc5459ddbaebf63573838770c5dbdae1a
Richard Neumann <mail@richard-neumann.de>
2021-03-31 14:16:29 +0200

    Migrate to setuptools and scm

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='backlight',
-    version='latest',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
@@ -12,5 +14,12 @@ setup(
     license='GPLv3',
     packages=['backlight', 'backlight.api'],
     scripts=['files/backlight', 'files/backlightd'],
+    entry_points={
+        'console_scripts': [
+            'backlight = backlight.cli:spawn',
+            'backlightd = backlight.daemon:main'
+        ]
+    },
     data_files=[('/usr/share/licenses/backlight/', ['LICENSE'])],
-    description='A screen backlight API and daemon.')
+    description='A screen backlight API and daemon.'
+)
